85656ad7daef3815ab2bad02a397bd52f05b49d0
poteto <naonyan1135@gmail.com>
2024-03-18 22:43:52 +0900

    暗号化関数の改善と日本語ロケールの対応

--- cipher.php
+++ cipher.php
@@ -1,17 +1,20 @@
 <?php
 header('Content-Type: text/plain; charset=UTF-8');
 
-$secret_key = 'secretangou';
+$secret_key = 'secretphpscriptqaqaqsaaaaaaaaa'; // 32 bytes key for AES-256-CBC
 $iv = '1234567890123456'; // 16 bytes IV for AES-256-CBC
 
 function encrypt($text, $secret_key, $iv) {
-    return openssl_encrypt($text, 'AES-256-CBC', $secret_key, 0, $iv);
+    $encrypted = openssl_encrypt($text, 'AES-256-CBC', $secret_key, OPENSSL_RAW_DATA, $iv);
+    return base64_encode($encrypted);
 }
 
 function decrypt($text, $secret_key, $iv) {
-    return openssl_decrypt($text, 'AES-256-CBC', $secret_key, 0, $iv);
+    $decrypted = openssl_decrypt(base64_decode($text), 'AES-256-CBC', $secret_key, OPENSSL_RAW_DATA, $iv);
+    return $decrypted;
 }
 
+// @index.phpからのリクエストを受け取る
 $text = $_POST['text'];
 $operation = $_POST['operation'];
 
